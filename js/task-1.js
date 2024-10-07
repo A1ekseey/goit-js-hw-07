4ba4458438baf19e72813d2404668190a64123a2
A1ekseey <132073015+A1ekseey@users.noreply.github.com>
2024-10-07 22:29:22 +0300

    js  7 (1.5)

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,11 @@
-const categoriesList = document.querySelectorAll("#categories .item");
-categoriesList.forEach((category) => {
-  const categoryTitle = category.querySelector("h2").textContent;
-  const categoryItems = category.querySelectorAll("ul li").length;
-  console.log(categoryTitle);
-  console.log(categoryItems);
-});
+function firstTask() {
+  const categories = document.querySelector('#categories');
+  const categoriesList = categories.children;
+  console.log('Number of categories: ', categoriesList.length);
+  for (const child of categoriesList) {
+    console.log('Category: ', child.firstElementChild.textContent);
+    console.log('Elements: ', child.lastElementChild.children.length);
+  }
+}
+
+firstTask();
