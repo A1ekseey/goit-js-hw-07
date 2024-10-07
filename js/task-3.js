a28e1fc267ddcbb5139b9ec1e7f094f06c399265
A1ekseey <132073015+A1ekseey@users.noreply.github.com>
2024-10-07 20:27:33 +0300

    js 7 (1.1)

--- js/task-3.js
+++ js/task-3.js
@@ -1,33 +1,6 @@
-class StringBuilder {
-    #value;
-  
-    constructor(initialValue) {
-      this.#value = initialValue;
-    }
-  
-    getValue() {
-      return this.#value;
-    }
-  
-    padEnd(str) {
-      this.#value += str;
-    }
-  
-    padStart(str) {
-      this.#value = str + this.#value;
-    }
-  
-    padBoth(str) {
-      this.padStart(str);
-      this.padEnd(str);
-    }
-  }
-  
-  const builder = new StringBuilder(".");
-  console.log(builder.getValue()); // "."
-  builder.padStart("^");
-  console.log(builder.getValue()); // "^."
-  builder.padEnd("^");
-  console.log(builder.getValue()); // "^.^"
-  builder.padBoth("=");
-  console.log(builder.getValue()); // "=^.^="
+const nameInput = document.querySelector("#name-input");
+const nameOutput = document.querySelector("#name-output");
+nameInput.addEventListener("input", () => {
+  const inputValue = nameInput.value.trim();
+  nameOutput.textContent = inputValue === `` ? `Anonymous` : inputValue;
+});
